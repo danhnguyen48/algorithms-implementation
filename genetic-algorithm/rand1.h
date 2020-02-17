376be03cc685972da03a8576108c587edce31795
Danh Nguyen <danh.nghuu@gmail.com>
2020-02-17 01:15:46 +0100

    update genetic

---
+++ genetic-algorithm/rand1.h
@@ -0,0 +1,19 @@
+#define IA 16807
+#define IM 2147483647
+#define AM (1.0/IM)
+#define IQ 127773
+#define IR 2836
+#define NTAB 32
+#define NDIV (1+(IM-1)/NTAB)
+#define EPS 1.2e-7
+#define RNMX (1.0-EPS)
+
+
+float ran1(long* );
+/*
+“Minimal” random number generator of Park and Miller with Bays-Durham shuffle and added
+safeguards. Returns a uniform random deviate between 0.0 and 1.0 (exclusive of the endpoint
+values). Call with idum a negative integer to initialize; thereafter, do not alter idum between
+successive deviates in a sequence. RNMX should approximate the largest floating value that is
+less than 1.
+*/
